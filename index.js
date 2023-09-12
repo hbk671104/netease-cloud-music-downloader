1b99260499543fd9a7c37740337631cf7a722e96
hbk671104 <hbk671104@gmail.com>
2023-09-12 16:05:28 +0800

    add env url

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 console.log('Hello via Bun!')
 
-const webUrl = 'http://localhost:3001'
-
-let playlist = await fetch(`${webUrl}/playlist/track/all?id=2044230836`)
+let playlist = await fetch(
+  `${Bun.env.NETEAST_CLOUD_URL}/playlist/track/all?id=2044230836`
+)
 playlist = await playlist.json()
 
 const ids = playlist.songs.map((song) => song.id).join(',')
 
-let res = await fetch(`${webUrl}/song/url?id=${ids}&br=320000`)
+let res = await fetch(
+  `${Bun.env.NETEAST_CLOUD_URL}/song/url?id=${ids}&br=320000`
+)
 res = await res.json()
 res = res.data.map((song) => song.url)
 
-await Bun.write('output.json', JSON.stringify(res))
+await Bun.write('./output/download-urls.txt', res.join('\n'))
